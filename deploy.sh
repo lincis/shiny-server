261beb8f77a7061094a69f9ed46b162fc9af92f4
Linards Kalvāns <linards.kalvans@gmail.com>
2019-10-21 22:39:18 +0300

    Correct private key filename

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-chmod 600 id_rsa
-scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml $1:~/shiny
-ssh -o StrictHostKeyChecking=no -i id_rsa $1 "cd ~/shiny; docker-compose stop shiny; docker-compose pull shiny; docker-compose up -d shiny"
+chmod 600 deploy_key
+scp -o StrictHostKeyChecking=no -i deploy_key docker-compose.yml $1:~/shiny
+ssh -o StrictHostKeyChecking=no -i deploy_key $1 "cd ~/shiny; docker-compose stop shiny; docker-compose pull shiny; docker-compose up -d shiny"
